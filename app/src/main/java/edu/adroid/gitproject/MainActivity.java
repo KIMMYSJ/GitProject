d4c7bf6979b234500c9ce8f7fd53f6feeb4da83d
KIMMYSJ <zun710@Hotmail.com>
2018-04-13 13:53:28 +0900

    버튼 찾음

--- app/src/main/java/edu/adroid/gitproject/MainActivity.java
+++ app/src/main/java/edu/adroid/gitproject/MainActivity.java
@@ -2,6 +2,8 @@ package edu.adroid.gitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        TextView textview = findViewById(R.id.textview);
+        TextView textview = findViewById(R.id.textView);
         Button button = findViewById(R.id.button);
     }
 }
